3edd962855fdb70ca68fb72fd5726b97e10f6ccb
SerjeelRanjan <serjeel.ranjan@gmail.com>
2021-03-15 22:04:21 +0530

    full volume now

--- public/js/app-index.js
+++ public/js/app-index.js
@@ -1,10 +1,10 @@
 const spiral = document.querySelector(".spiral");
 
 //set the default width and height of the svg
-const dimension = parseInt(Math.max(screen.width,screen.height)*1.3);
+const dimension = parseInt(Math.max(screen.width, screen.height) * 1.3);
 console.log(dimension);
-spiral.style.height = dimension+"px";
-spiral.style.width = dimension+"px";
+spiral.style.height = dimension + "px";
+spiral.style.width = dimension + "px";
 
 //connecting to the sliders
 // document.querySelector(".slider__dimension").oninput = function () {
@@ -13,25 +13,23 @@ spiral.style.width = dimension+"px";
 
 //playing the sound
 
-
-document.querySelector(".toggle").addEventListener("click",()=>{
+document.querySelector(".toggle").addEventListener("click", () => {
     const btn = document.querySelector(".toggle__icon");
-    if(btn.id === "playing"){
+    if (btn.id === "playing") {
         console.log("stopped play");
         const audio = document.querySelector("audio");
         audio.pause();
         btn.src = "/css/img/sound_close.png";
-        btn.removeAttribute('id');
-    }   
-    else if (document.readyState === "complete" || document.readyState === "loaded" ) {
+        btn.removeAttribute("id");
+    } else if (document.readyState === "complete" || document.readyState === "loaded") {
         console.log("started play");
         const audio = document.querySelector("audio");
         btn.src = "/css/img/sound_open.png";
-        audio.volume = 0.2;
-        btn.id = "playing"
+        audio.volume = 1;
+        btn.id = "playing";
         audio.play();
-   }
-}); 
+    }
+});
 
 //DOM functions
 // changeDimension = newDimension => {
